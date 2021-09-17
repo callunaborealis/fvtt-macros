d151f34860339f8c939631c981e63f9b99f59398
callunaborealis <callunaborealis@gmail.com>
2021-09-17 20:37:30 +0800

    Ensure more macros can be added

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
-import autoprefixer from "autoprefixer";
-import postcss from "rollup-plugin-postcss-modules";
+// import autoprefixer from "autoprefixer";
+// import postcss from "rollup-plugin-postcss-modules";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "src/macros.ts",
+  input: {
+    "witcher/defense": "src/witcher/defense/index.ts",
+  },
   output: {
     dir: "build",
+    entryFileNames: "[name].js",
     format: "es",
     preserveModules: false, // Keep directory structure and files
     preserveModulesRoot: "src",
@@ -15,15 +18,15 @@ export default {
   },
   plugins: [
     resolve({ browser: true }),
-    postcss({
-      extract: true,
-      modules: {
-        generateScopedName: "mm3e__[folder]__[name]__[local]--[hash:base64:5]",
-      },
-      minimize: true,
-      plugins: [autoprefixer()],
-      writeDefinitions: false, // Writing automatic definitions will freeze up the rollup build
-    }),
+    // postcss({
+    //   extract: true,
+    //   modules: {
+    //     generateScopedName: "mm3e__[folder]__[name]__[local]--[hash:base64:5]",
+    //   },
+    //   minimize: true,
+    //   plugins: [autoprefixer()],
+    //   writeDefinitions: false, // Writing automatic definitions will freeze up the rollup build
+    // }),
     json(),
     typescript(),
   ],
--- src/typings.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.scss" {
-  type ClassName = string;
-  const classNames: Record<ClassName, string>;
-  export default classNames;
-}
